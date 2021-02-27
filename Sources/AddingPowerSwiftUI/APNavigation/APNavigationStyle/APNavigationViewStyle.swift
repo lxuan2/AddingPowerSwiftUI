41e2278c585df3beb79b1f38fca85fbc2f6f2e80
lxuan2 <lxuan2@vt.edu>
2021-02-26 23:32:04 -0500

    v0.3.5

--- Sources/AddingPowerSwiftUI/APNavigation/APNavigationStyle/APNavigationViewStyle.swift
+++ Sources/AddingPowerSwiftUI/APNavigation/APNavigationStyle/APNavigationViewStyle.swift
@@ -6,10 +6,14 @@
 
 import SwiftUI
 
+// MARK: - APNavigationViewStyle
+
 public protocol APNavigationViewStyle {
     associatedtype Body : View
     func makeBody(configuration: APNavigationViewStyleConfiguration) -> Body
 }
+
+// MARK: - APNavigationViewStyleConfiguration
 
 public struct APNavigationViewStyleConfiguration {
     public struct Content: View {
@@ -24,3 +28,26 @@ public struct APNavigationViewStyleConfiguration {
 }
 
 extension APNavigationViewStyleConfiguration.Content: APStaticView {}
+
+// MARK: - APNavigationViewStyleKey
+
+struct APNavigationViewStyleKey: APStyleKey {
+    static func makeDefault(configuration: APNavigationViewStyleConfiguration) -> some View {
+        APStackNavigationViewStyle().makeBody(configuration: configuration)
+    }
+}
+
+// MARK: - APNavigationViewStyleModifier
+struct APNavigationViewStyleModifier<Style: APNavigationViewStyle>: ViewModifier {
+    let style: Style
+    
+    func body(content: Content) -> some View {
+        content.style(APNavigationViewStyleKey.self, style.makeBody)
+    }
+}
+
+extension View {
+    public func apNavigationViewStyle<S: APNavigationViewStyle>(_ style: S) -> some View {
+        modifier(APNavigationViewStyleModifier(style: style))
+    }
+}
